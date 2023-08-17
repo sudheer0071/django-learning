c88ed37d7cc41db102ee8540808b794842b82791
sudheer0071 <sudheer1614@gmail.com>
2023-08-17 21:49:11 +0530

    changes

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 # from blog.views import home
+from users import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("register/", user_views.register, name='register'),
     path("", include('blog.urls')),
     # path('blog', home)
 ]
