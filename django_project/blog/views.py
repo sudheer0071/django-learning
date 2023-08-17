872c47f92d5cb3b54b1c1d8f42b8f779ab399879
sudheer0071 <sudheer1614@gmail.com>
2023-08-17 21:14:02 +0530

    updating

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-# Create your views here.
+# Create your views here
 
 def home(request):
     context = {
