5740a239cc5e5442636d2f9b02bfc2af9aa770ec
sudheer0071 <sudheer1614@gmail.com>
2023-08-19 14:44:38 +0530

    new changes

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -6,13 +6,22 @@ from django.http import HttpResponse
 
 posts  = [
     {
-        
-    }
+        'author': 'Dummy1',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted': 'August 27 2018'
+    },
+    {
+        'author': 'Dummy2',
+        'title': 'Blog Post 2',
+        'content':'second post content',
+        'date_posted': 'August 28 2018'
+    },
 ]
 
 def home(request):
     context={
-        'posts':Post.objects.all()
+        'posts':posts
     }
     return render(request,'blog/home.html',context)
 
