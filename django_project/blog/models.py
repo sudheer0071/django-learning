a3399eb6e208cbf281c2e4f584cacd2c20609af6
sudheer0071 <sudheer1614@gmail.com>
2023-08-19 19:56:01 +0530

    working test

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -6,5 +6,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    data_posted = models.DateTimeField(default=timezone.now)
+    data_pdfosted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete= models.CASCADE) 
